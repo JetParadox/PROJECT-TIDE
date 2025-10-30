0c1ea38ea60eb1ea343c5b04cb25814926e49f41
Akhil M Mathew <66257999+TheRAVAGE@users.noreply.github.com>
2025-10-30 20:18:26 +0000

    [SETUP] : Added Levels, files and Folders
    -Added Few more Test Levels and put them in a seperate folder
    -Added more Folders for Models, Animations Etc
    -Added a initial C++ Script to test
    -Setup lfs
    -Added .gitignore and updated .gitattributes

---
+++ Source/PROJECT_TIDE/PROJECT_TIDE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PROJECT_TIDE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PROJECT_TIDE, "PROJECT_TIDE" );
